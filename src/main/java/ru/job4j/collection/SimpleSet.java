9cbf039274d30878d0eef70248113db7c365e8cf
Evgenii <job4jstudy@yandex.ru>
2021-09-14 16:42:29 +1000

    SimpleSet

---
+++ src/main/java/ru/job4j/collection/SimpleSet.java
@@ -0,0 +1,44 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+/**
+ * SimpleSet
+ * @author Evgenii Shegai
+ * @since 14.09.2021
+ * @version 1.0
+ */
+
+public class SimpleSet<T> implements Set<T> {
+
+    private SimpleArray array = new SimpleArray(16);
+
+    public SimpleSet(SimpleArray array) {
+        this.array = array;
+    }
+
+    public boolean add(T value) {
+        boolean result = false;
+            if (!contains(value)) {
+                array.add(value);
+                result = true;
+            }
+            return result;
+        }
+
+        public boolean contains(T value) {
+            Iterator<T> it = array.iterator();
+            while (it.hasNext()) {
+                if (Objects.equals(value, it.next())) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public Iterator<T> iterator() {
+            return array.iterator();
+        }
+}
---
+++ src/test/java/ru/job4j/collection/SimpleSetTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * SimpleSet
+ * @author Evgenii Shegai
+ * @since 14.09.2021
+ * @version 1.0
+ */
+
+public class SimpleSetTest {
+
+    private SimpleArray array = new SimpleArray<>(16);
+    private SimpleSet set = new SimpleSet<>(array);
+
+    @Before
+    public void whenCreateSet() {
+        array.add("one");
+        array.add("six");
+        array.add("two");
+    }
+
+    @Test
+    public void whenAddDuplicateThen() {
+        boolean result = set.add("six");
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenAddNonNull() {
+        Set<Integer> set = new SimpleSet<>(new SimpleArray(16));
+        assertTrue(set.add(1));
+        assertTrue(set.contains(1));
+        assertFalse(set.add(1));
+    }
+
+    @Test
+    public void whenAddNull() {
+        Set<Integer> set = new SimpleSet<>(new SimpleArray(16));
+        assertTrue(set.add(null));
+        assertTrue(set.contains(null));
+        assertFalse(set.add(null));
+    }
+
+}
