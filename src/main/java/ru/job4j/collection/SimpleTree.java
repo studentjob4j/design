eea4614a632ba52ebfe21f61ece5bca2e1b1428f
Evgenii <job4jstudy@yandex.ru>
2021-09-15 22:21:24 +1000

    SimpleTree is binary or not

--- src/main/java/ru/job4j/collection/SimpleTree.java
+++ src/main/java/ru/job4j/collection/SimpleTree.java
@@ -3,6 +3,7 @@ package ru.job4j.collection;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.function.Predicate;
 
 /**
  * SimpleTree
@@ -40,12 +41,21 @@ public class SimpleTree<E> implements Tree<E> {
 
     @Override
     public Optional<Node<E>> findBy(E value) {
+       return findByPredicate(x ->
+          x.value.equals(value));
+    }
+
+    public boolean isBinary() {
+        return !findByPredicate(x -> x.children.size() > 2).isPresent();
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
@@ -53,4 +63,5 @@ public class SimpleTree<E> implements Tree<E> {
         }
         return rsl;
     }
+
 }
