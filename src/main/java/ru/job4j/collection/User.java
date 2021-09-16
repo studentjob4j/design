e00bb75aaf17a079bb8a93a4a51bdd520bd07158
Evgenii <job4jstudy@yandex.ru>
2021-09-16 16:19:54 +1000

    HashMap  equals and hashcode разные варианты

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -1,12 +1,17 @@
 package ru.job4j.collection;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
- * Без переопределения иквалс и хешкод два объекта имеют разный хеш и лежат в разных бакетах
+ * Без переопределения equals and hashcode   два объекта имеют разный хеш и лежат в разных бакетах
+ *
+ *  Без переопределения иквалс - два объекта имеют одинаковый хеш но лежат в разных бакетах
+ * т.к в hashmap у них разные хеши .
+ *
+ * Переопределить только equals - hashcode разный значит объекты попали в разные бакеты и equals
+ * не вызывался
+ *
+ * Когда переопределил equals and hashcode то в карте всео один объект
  * @author Evgenii Shegai
  * @since 16.09.2021
  * @version 1.0
@@ -24,15 +29,33 @@ public class User {
         this.birthday = birthday;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name)
+                && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
     public static void main(String[] args) {
-        Calendar calendar = new GregorianCalendar(2000, 5, 21);
+        Calendar calendar = new GregorianCalendar(2000, 5, 21, 11, 12, 22);
         User one = new User("Natasha", 2, calendar);
         User two = new User("Natasha", 2, calendar);
         System.out.println(one.hashCode());
         System.out.println(two.hashCode());
         Map<User, Object> map = new HashMap<>();
-        map.put(one, 1);
-        map.put(two, 2);
+        map.put(one, new Object());
+        map.put(two, new Object());
         System.out.println(map.size());
         for (Map.Entry entry : map.entrySet()) {
             System.out.println("Key: " + entry.getKey() + " Value: "
