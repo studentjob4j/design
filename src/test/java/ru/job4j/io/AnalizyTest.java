e0371643800d9c7a4de61a4f8caf1edd9d2418ab
Evgenii <job4jstudy@yandex.ru>
2021-09-18 20:05:58 +1000

    Тестирование IO

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,8 +1,9 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
-import java.io.BufferedReader;
-import java.io.FileReader;
+import org.junit.rules.TemporaryFolder;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import static org.hamcrest.core.Is.is;
@@ -16,6 +17,13 @@ import static org.junit.Assert.*;
  */
 
 public class AnalizyTest {
+
+
+    /**
+     * класс TemporaryFolder нужно использовать, когда программа записывает результат в файл.
+     */
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
     public void whenServerNotWorkSomeTime() {
@@ -35,5 +43,38 @@ public class AnalizyTest {
             e.printStackTrace();
         }
         assertThat(result, is(expect));
+    }
+
+    /**
+     * Чтобы создать файлы во временной директории, нужно использовать класс org.unit.rules.TemporaryFolder.
+     * Этот класс позволяет создавать файлы и директории во временном каталоге.
+     * После запуска теста файлы созданные через TemporaryFolder будут удалены.
+     * Теперь нам нет необходимости заботиться о мусоре, который оставляет наш тест, потому что его
+     * уберет класс  TemporaryFolder.
+     */
+
+    @Test
+    public void whenUseTemporaryFolder() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        // читаем файл из лога и записываем его далее в источник
+        try (PrintWriter out = new PrintWriter(source);
+             BufferedReader in = new BufferedReader(new FileReader("data/log.txt"))) {
+            while (in.ready()) {
+                out.write(in.read());
+            }
+        }
+
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        // читаем из таргета и добавляем в билдер
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(x -> rsl.append(x + System.lineSeparator()));
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append("10:57:01;10:59:01" + System.lineSeparator());
+        builder.append("11:01:02;11:02:02" + System.lineSeparator());
+        assertThat(rsl.toString(), is(builder.toString()));
     }
 }
